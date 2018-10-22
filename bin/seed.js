32202d411847fe14f3b4cc8d8313e3c0fcf6a849
Luke <lucasbepps@gmail.com>
2018-10-22 15:54:04 -0400

    first commit

---
+++ bin/seed.js
@@ -0,0 +1,42 @@
+require('mongoose').connect('mongodb://localhost/pollser');
+
+const topics = [
+    "Should dogs be allowed to fly?",
+    "Should doors be shut at night?",
+    "Should developers use IDEs?",
+    "Should cars have four wheels?",
+    "Should humans be allowed to wear shoes?"
+];
+
+let Poll = require('../models/poll');
+
+// empty collection first
+Poll.remove({}).then(() => {
+        let polls = [];
+        for (let i = 0; i < 5; i++) {
+            polls.push({
+                topic: topics[i],
+                choices: [{
+                        value: "Yes",
+                        votes: Math.round(Math.random() * 20)
+                    },
+                    {
+                        value: "No",
+                        votes: Math.round(Math.random() * 20)
+                    },
+                    {
+                        value: "I really don't care",
+                        votes: Math.round(Math.random() * 20)
+                    }
+                ]
+            });
+        }
+        return Poll.create(polls);
+    })
+    .then(() => {
+        process.exit();
+    })
+    .catch((e) => {
+        console.log(e);
+        process.exit(i);
+    });
---
+++ models/poll.js
@@ -0,0 +1,11 @@
+let mongoose = require('mongoose');
+
+let Poll = mongoose.model('Poll', {
+    topic: String,
+    choices: [{
+        value: String,
+        votes: Number
+    }]
+});
+
+module.exports = Poll;
